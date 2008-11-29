809dbb064a2d2c10c21f656b361bd4da56361949
Takashi Matsuo <tmatsuo@shehas.net>
2008-11-29 22:13:30 +0900

    Added url() call to urls.py.

--- gheimdall2/urls.py
+++ gheimdall2/urls.py
@@ -1,26 +1,17 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-# Uncomment the next two lines to enable the admin:
-#from django.contrib import admin
-#admin.autodiscover()
 
 urlpatterns = patterns(
   'gheimdall2.idp.views',
-  # Example:
-  # (r'^gheimdall2/', include('gheimdall2.foo.urls')),
-  # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-  # to INSTALLED_APPS to enable admin documentation:
-  #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-  # Uncomment the next line to enable the admin:
-  #(r'^admin/(.*)', admin.site.root),
-  (r'^static_login$', 'static_login'),
-  (r'^login$', 'login'),
-  (r'^login.do$', 'login_do'),
-  (r'^logout$', 'logout'),
-  (r'^passwd$', 'password'),
-  (r'^passwd.do$', 'passwd_do'),
-  (r'^admin/reset_password$', 'reset_password'),
-  (r'^admin/reset_password.do$', 'reset_password_do'),
+  url(r'^static_login$', 'static_login', name='static_login'),
+  url(r'^login$', 'login', name='login'),
+  url(r'^login.do$', 'login_do', name='login_do'),
+  url(r'^logout$', 'logout', name='logout'),
+  url(r'^passwd$', 'password', name='passwd'),
+  url(r'^passwd.do$', 'passwd_do', name='passwd_do'),
+  url(r'^admin/reset_password$', 'reset_password', name='reset_password'),
+  url(r'^admin/reset_password.do$', 'reset_password_do',
+      name='reset_password_do'),
 )
 
 if settings.DEBUG:
